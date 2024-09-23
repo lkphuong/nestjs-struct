62319ff0b8b78e127f61d6972df53846b5ff2c97
lkphuong <lekhphuong@gmail.com>
2024-09-23 22:17:18 +0700

    refactor: Update main.ts to add logging, CORS, global prefix, and Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
-import { NestFactory } from '@nestjs/core';
+import {
+  BaseExceptionFilter,
+  HttpAdapterHost,
+  NestFactory,
+} from '@nestjs/core';
+
+import * as morgan from 'morgan';
+import * as Sentry from '@sentry/nestjs';
+
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan('dev'));
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET, POST, PUT, DELETE'],
+  });
+
+  app.setGlobalPrefix('api');
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  Sentry.setupNestErrorHandler(app, new BaseExceptionFilter(httpAdapter));
+
+  const config = new DocumentBuilder()
+    .setTitle('Example API')
+    .setDescription('The example API description')
+    .setVersion('1.0')
+    .addTag('example')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('document', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
